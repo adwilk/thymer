40e65611dd81c99da6fbe1307d70f8a3f0265f21
Adam Wilkinson <adwilk@adwilk-2.local>
2014-12-24 14:52:53 +0000

    init

---
+++ thyme.js
@@ -0,0 +1,43 @@
+var Q = require('q');
+
+var thyme = {
+	deffered:Q.defer(),
+	timeout:null,
+	interval:null,
+	limit:null,
+	wait: function(n){
+		this.timeout = n;
+		return this;
+	},
+	every: function(n){
+		this.interval = n;
+		return this;
+	},
+	for: function(n){
+		this.limit = n;
+		return this.interval ? this : console.error('every for needs an every');
+	},
+	do: function(f){
+		var self = this;
+		var goal = function(){
+			var sub = function(){
+				f();
+				self.limit ? self.limit-- : undefined;
+				self.limit === 0 ? self.stop() : undefined;
+			}
+			self.interval ? self.interval = setInterval(sub, self.interval) : sub();
+		}
+		this.timeout ? setTimeout(goal, this.timeout) : goal();
+		return this.deffered.promise;
+	},
+	stop: function(){
+		clearInterval(this.interval);
+		this.deffered.resolve();
+	}
+}
+
+thyme.wait(5000).every(1000).for(3).do(function(){
+	console.log('tick');
+}).then(function(){
+	console.log('boom');
+});
