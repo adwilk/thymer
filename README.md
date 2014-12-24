f551dd1d0651223c10141db7cbc8a8b7f3cfc0f5
Adam Wilkinson <adwilk@adwilk-2.local>
2014-12-24 14:58:48 +0000

    init

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-'''
+```
+var thyme = require('thyme');
+
 thyme.wait(5000).every(1000).for(3).do(function(){
 	console.log('tick');
 }).then(function(){
 	console.log('boom');
 });
-'''
+```
--- thyme.js
+++ thyme.js
@@ -35,9 +35,3 @@ var thyme = {
 		this.deffered.resolve();
 	}
 }
-
-thyme.wait(5000).every(1000).for(3).do(function(){
-	console.log('tick');
-}).then(function(){
-	console.log('boom');
-});
